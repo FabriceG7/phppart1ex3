4a6b796bd44578cc93fef5c8fea9e99f95258fa0
FabriceG7 <goret.goret@sfr.fr>
2020-05-22 12:26:53 +0200

    phppart1ex3

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$km = 1;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8/">
+    <title>Exercice3</title>
+  </head>
+  <body>
+    <h1>
+      <?php
+      echo $km ;
+      ?>
+      <?php
+      $km = 3 ;
+      echo $km ;
+      ?>
+      <?php
+      $km = 125 ;
+      echo $km ;
+      ?>
+    </h1>
+  </body>
+</html>
